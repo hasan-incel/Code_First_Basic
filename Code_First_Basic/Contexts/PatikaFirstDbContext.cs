d6f0419cdc318ecd372a1ba132169f6831b6b71b
Hasan İncel <h_incel@hotmail.com>
2024-10-05 16:49:33 +0300

    Add project files.

---
+++ Code_First_Basic/Contexts/PatikaFirstDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Code_First_Basic.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Code_First_Basic.Contexts
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movies");
+            modelBuilder.Entity<Game>().ToTable("Games");
+        }
+    }
+}
